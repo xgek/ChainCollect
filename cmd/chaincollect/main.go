7a09eeb2d052f34883d271ca4a392ceca2d45d7c
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 21:26:16 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/chaincollect/main.go
@@ -0,0 +1,23 @@
+// cmd/chaincollect/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"chaincollect/internal/chaincollect"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := chaincollect.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
